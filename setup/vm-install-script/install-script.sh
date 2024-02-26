b44841b0f862036d56be5192d3b10b31d3b15937
yhDananjaya <160813471+yhDananjaya@users.noreply.github.com>
2024-02-26 18:53:38 +0530

    Update install-script.sh
    Jenkins public key updating commands added

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -72,6 +72,8 @@ mvn -v
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install -y jenkins
 systemctl daemon-reload
